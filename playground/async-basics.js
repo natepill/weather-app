909564119026d5ce2a44906fa683a0689b2df9f1
Nathan Pillai <natepill@gmail.com>
2018-10-23 17:03:22 -0700

    first commit, playground folder

---
+++ playground/async-basics.js
@@ -0,0 +1,12 @@
+console.log('Starting app')
+
+setTimeout(()=> {
+    console.log('Inside of callback');
+}, 2000);
+
+setTimeout(() =>{
+
+    console.log('Zero seconds');
+}, 0)
+
+console.log('Finishing up')
